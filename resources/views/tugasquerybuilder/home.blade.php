b353f23700979ba821129b5205b7bacfb06939ff
MUHAMMAD ARMANDO NUR RIZQY ANSAR <87767318+armando2326@users.noreply.github.com>
2021-11-25 14:19:15 +0700

    Tugas PWEB Query Builder
    Tugas PWEB Query Builder 25 Nov 2021 Table tugas with Decorations

--- resources/views/tugasquerybuilder/home.blade.php
+++ resources/views/tugasquerybuilder/home.blade.php
@@ -2,41 +2,72 @@
 <html>
 <head>
 	<title>Tugas PWEB Query Builder</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color: #2E4C6D;
+    }
+
+    td, th {
+        padding: 10px;
+        text-align: center;
+    }
+
+    .card {
+        border-radius: 25px;
+        box-shadow: 7px 7px 10px 5px rgba(0, 0, 0, 0.05);
+    }
+
+    </style>
 </head>
 <body>
+    <div class="row justify-content-center no-gutters">
+        <div class="col-sm-8">
+            <div class="card mt-5 p-sm-5">
 
-	<h3>Data Tugas</h3>
+            <h3 style="text-align: center">Data Tugas</h3>
 
-	<a href="/tugas/tambah"> + Tambah Tugas Baru</a>
+            <div class="row mt-3">
+                <div class="col-sm-5">
+	                <a class="btn btn-primary" href="/tugas/tambah"> + Tambah Tugas Baru</a>
+                </div>
+            </div>
 
-	<br/>
-	<br/>
+	        <br/>
 
-	<table border="1">
-		<tr>
-            <th>ID</th>
-			<th>ID Pegawai</th>
-			<th>Tanggal</th>
-			<th>Nama Tugas</th>
-			<th>Status</th>
-            <th>Opsi</th>
-		</tr>
-		@foreach($tugas as $t)
-		<tr>
-            <td>{{ $t->ID }}</td>
-			<td>{{ $t->IDPegawai }}</td>
-			<td>{{ $t->Tanggal }}</td>
-			<td>{{ $t->NamaTugas }}</td>
-			<td>{{ $t->Status }}</td>
-			<td>
-				<a href="/tugas/edit/{{ $t->ID }}">Edit</a>
-				|
-				<a href="/tugas/hapus/{{ $t->ID }}">Hapus</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>ID Pegawai</th>
+                    <th>Tanggal</th>
+                    <th>Nama Tugas</th>
+                    <th>Status</th>
+                    <th>Opsi</th>
+                </tr>
+                @foreach($tugas as $t)
+                <tr>
+                    <td>{{ $t->ID }}</td>
+                    <td>{{ $t->IDPegawai }}</td>
+                    <td>{{ date('Y-m-d g:i A', strtotime($t->Tanggal)) }}</td>
+                    <td style="width: 350px;">{{ $t->NamaTugas }}</td>
+                    <td>{{ $t->Status }}</td>
+                    <td>
+                        <a class="btn btn-warning btn-sm mr-1" href="/tugas/edit/{{ $t->ID }}">Edit</a>
+                        <a class="btn btn-danger btn-sm" href="/tugas/hapus/{{ $t->ID }}">Hapus</a>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
--- resources/views/tugasquerybuilder/tambah.blade.php
+++ resources/views/tugasquerybuilder/tambah.blade.php
@@ -2,24 +2,64 @@
 <html>
 <head>
 	<title>Tambah Data Tugas</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color: #2E4C6D;
+    }
+
+    .card {
+        border-radius: 25px;
+        box-shadow: 7px 7px 10px 5px rgba(0, 0, 0, 0.05);
+    }
+    </style>
 </head>
 <body>
 
-	<h3>Data Pegawai</h3>
+	<div class="row justify-content-center no-gutters">
+        <div class="col-sm-5">
+            <div class="card mt-5 p-sm-5">
 
-	<a href="/tugas"> Kembali</a>
+            <h3 style="text-align: center">Tambah Tugas Pegawai</h3>
 
-	<br/>
-	<br/>
+            <div class="row mt-3">
+                <div class="col-sm-5">
+                    <a class="btn btn-danger btn-sm mt-3" href="/tugas"> Kembali</a>
+                </div>
+            </div>
 
-	<form action="/tugas/store" method="post">
-		{{ csrf_field() }}
-		ID Pegawai <input type="number" name="idpegawai" required="required"> <br/>
-		Tanggal <input type="datetime-local" name="tanggal" required="required"> <br/>
-		Nama Tugas <textarea name="namatugas" required="required"></textarea> <br/>
-		Status <input type="text" name="status" required="required"> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
+            <br>
+
+            <form action="/tugas/store" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="idpegawai">ID Pegawai</label>
+                    <input class="form-control" type="number" id="idpegawai" name="idpegawai" required="required">
+                </div>
+                <div class="form-group">
+                    <label for="idpegawai">Tanggal</label>
+                    <input class="form-control" type="datetime-local" id="tanggal" name="tanggal" required="required">
+                </div>
+                <div class="form-group">
+                    <label for="namatugas">Nama Tugas</label>
+                    <textarea class="form-control" id="namatugas" name="namatugas" required="required" maxlength="50"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="status">Status</label>
+                    <input class="form-control" type="text" id="status" name="status" required="required" maxlength="1">
+                </div>
+
+                <input class="btn btn-primary" type="submit" value="Simpan Data">
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
